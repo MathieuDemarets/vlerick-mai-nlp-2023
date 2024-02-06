1f199f11347b3655f82888a255e83a38d84e188d
MathieuDemarets <74194884+MathieuDemarets@users.noreply.github.com>
2024-02-06 14:55:16 +0000

    final web app

--- Final_Assignment/app.py
+++ Final_Assignment/app.py
@@ -7,6 +7,14 @@ import numpy as np
 import time
 import keyboard
 import psutil
+import fitz
+import re
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
 
 
 def SemanticSearchEngine_N(
@@ -76,7 +84,101 @@ def SemanticSearchEngine_N(
     return retrieved.loc[:, ["title", rank, "pages", "abstract", "path"]]
 
 
+def extract_abstract(article, char_max=1000):
+    """Extract the abstract from a pdf file
+
+    Parameters
+    ----------
+    path : str
+        Path to the pdf file
+    char_max : int, optional
+        Maximum number of characters to extract if introduction not found, by default 1000
+
+    Returns
+    -------
+    str
+        Abstract of the pdf file
+    """
+    with fitz.open(stream=article.read(), filetype="pdf") as doc:
+        text = ""
+        # Add two extra pages to the text
+        for i in range(3):
+            text += doc[i].get_textpage().extractText().lower()
+        # Find the abstract with white spaces inbetween characters
+        ret = None
+        s = 0
+        while not ret and s < 3:
+            search = "a.{"+str(s)+"}b.{"+str(s)+"}s.{"+str(s)+"}t.{" + \
+                str(s)+"}r.{"+str(s)+"}a.{"+str(s)+"}c.{"+str(s)+"}t"
+            ret = re.search(search, text)
+            s += not ret
+        abstract_unbounded = re.split(search, text)[1]
+        # Find the introduction with white spaces inbetween characters
+        ret = None
+        s = 0
+        while not ret and s < 3:
+            search = "i.{"+str(s)+"}n.{"+str(s)+"}t.{"+str(s)+"}r.{"+str(s)+"}o.{"+str(s)+"}d.{"+str(s)+"}u.{"+str(s) +\
+                "}c.{"+str(s)+"}t.{"+str(s)+"}i.{"+str(s)+"}o.{"+str(s)+"}n"
+            ret = re.search(search, text)
+            i += not ret
+        if not ret:
+            abstract = abstract_unbounded[:char_max]
+        abstract = re.split(search, abstract_unbounded)[0]
+        return abstract
+
+
+embeddings = pd.read_pickle("embeddings.pkl")
+index = pd.read_excel("kb_articles/AAA_index.xlsx")
+chosen_embedding = embeddings["msmarco-distilbert-base-tas-b"]
+# write the chosen embedding to a dataframe
+embedding_space = pd.DataFrame(
+    chosen_embedding.tolist(), index=chosen_embedding.index)
+embedding_space.columns = [f"dim_{i}" for i in embedding_space.columns]
+# dimensionality reduction to 2D
+preprocessing_pipe = make_pipeline(StandardScaler(), PCA(n_components=2))
+preprocessing_pipe.fit(embedding_space)
+embedding_space_2D = pd.DataFrame(preprocessing_pipe.transform(embedding_space)).rename(
+    columns={0: "PC1", 1: "PC2"})
+clusterer = KMeans(n_clusters=3, random_state=0)
+clusterer.fit(embedding_space_2D)
+
+model = SentenceTransformer("msmarco-distilbert-base-tas-b")
+
+
+def show_close_articles(abstract, n):
+    # Get the centroids of the clusters
+    centroids = clusterer.cluster_centers_
+    # Embed the abstract
+    emb_abstract = model.encode(abstract, convert_to_tensor=False)
+    red_emb_abstract = preprocessing_pipe.transform([emb_abstract])
+    # Get the closest articles
+    knn = NearestNeighbors(n_neighbors=n, metric="euclidean")
+    knn.fit(embedding_space_2D)
+    distances, indices = knn.kneighbors(red_emb_abstract)
+    # Plot the figure
+    fig, ax = plt.subplots()
+    ax.scatter(
+        embedding_space_2D.PC1, embedding_space_2D.PC2, c=clusterer.labels_,
+        cmap="viridis", alpha=0.5, s=15)
+    ax.scatter(
+        centroids[:, 0], centroids[:, 1], c="black", s=100, marker="X")
+    ax.scatter(
+        red_emb_abstract[:, 0], red_emb_abstract[:, 1], c="red", s=300, marker="*")
+    drawn_circle = plt.Circle(
+        (red_emb_abstract[:, 0], red_emb_abstract[:, 1]), distances.max(), color='r', fill=False)
+    fig.gca().add_artist(drawn_circle)
+    ax.set_xlabel('First dimension of the embedding space')
+    ax.set_ylabel('Second dimension')
+    ax.set_title('Clusters of the knowledge base')
+    return fig, index.loc[list(indices[0]), ["title", "abstract", "path"]]
+
+
 if __name__ == '__main__':
+
+    st.set_page_config(
+        page_title="Reacfin Knowledge Base",
+        page_icon=":books:")
+
     st.write("""
     # Reacfin Knowledge Base
     
@@ -88,9 +190,12 @@ if __name__ == '__main__':
 
     ---
     """)
+    if "article" not in st.session_state:
+        st.session_state["article"] = None
+
     query = st.text_input(
         "What do you want to know?",
-        "What are good performance metrics for credit default models?")
+        "What are P2P loans?")
     n = st.slider("Number of articles to retrieve:", 1, 10, 1)
     softmax = st.checkbox("Use softmax for ranking?")
     output_path = st.text_input(
@@ -101,10 +206,34 @@ if __name__ == '__main__':
             query, n=n, softmax=softmax, output_path=output_path)
         st.write(retrieved)
 
+    def change_article():
+        st.session_state["article"] = "uploaded"
+
+    st.write("""
+
+    ---
+
+    ## Find similar articles
+
+    Upload your PDF article to find similar ones in our collection. The search engine will use the abstract
+    to perform the search.
+    """)
+    uploaded_pdf = st.file_uploader(
+        "Upload your PDF file", type="pdf", on_change=change_article)
+    if st.session_state["article"] == "uploaded":
+        st.success("File uploaded successfully!")
+        abstract = extract_abstract(uploaded_pdf)
+        n_close = st.slider("Number of similar articles in radius:", 1, 5, 2)
+        if st.button("Show similar articles"):
+            fig, close_articles = show_close_articles(abstract, n_close)
+            st.pyplot(fig)
+            st.write(close_articles)
+
     # Solution to shut down the app inspired by: https://discuss.streamlit.io/t/close-streamlit-app-with-button-click/35132/4
-    if st.button("Shut Down"):
+    if st.sidebar.button("Shut Down"):
         # Give a bit of delay for user experience
-        st.write("Thank you for using our search engine!\nShutting down...")
+        st.sidebar.write(
+            "Thank you for using our search engine!\nShutting down...")
         time.sleep(3)
         # Close streamlit browser tab
         keyboard.press_and_release('ctrl+w')
